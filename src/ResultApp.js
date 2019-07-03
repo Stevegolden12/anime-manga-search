2782d5aa4a44e5e069a2906d06dd139b0b7a7cbf
Steve Golden <sgolden2525@outlook.com>
2019-07-03 03:25:15 -0500

    Redirecting results page
    Added express
    Added server.js file and others
    
    Added change to SelectionOption so that all html is wrapped in form and all tags are within one component.

---
+++ src/ResultApp.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+const axios = require('axios')
+
+class Results extends React.Component {
+	constructor(props) {
+		super(props);
+
+
+	}
+	render(){
+		return(
+			<div>
+			  <h1>Testing</h1>
+			</div>
+			)
+	}
+
+}
+
+
+export default ResultApp
